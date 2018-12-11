ecf1ff7ff8e4353494b10e20cfe97c9d23cb3455
Kaixong <bear851010@gmail.com>
2018-12-11 14:26:49 +0800

    1.1

--- DataGlove.sh
+++ DataGlove.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo "Welcome!"
+echo "### Welcome! ###"
 echo "You are about to begin 3 programs that control 5DT Glove."
-read -rsn1 -p"Press any key or wait for 3 seconds to continue..." -t 3 unm
+echo "### Make sure the ROSCORE have been opened. ###"
+read -rsn1 -p"Press any key or wait for 5 seconds to continue..." -t 5 unm
 password='shabadabadoo' #Don't record my keyword pls.
 
 #Change this varito your node name
-#node1_example: OnlyRight, Command, Gesture
-node1='dataglovetest dataglove_publisher_OnlyRight' 
+node1='dataglovetest dataglove_publisher_Gesture'
+    #node1_example: OnlyRight, Command, Gesture
 node2='ihand_control ihand_control_node'
 node3='pololu_maestro_servo pololu_maestro_servo_node'
 
